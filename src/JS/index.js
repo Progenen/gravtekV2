68d0e4a7459987ab64963fb5173e754566f56bd8
Stanislav Dombrovskiy <stanislav_2010@icloud.com>
2023-10-08 14:03:08 +0500

    gratek

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,11 +1,75 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // const burger = document.querySelector(".header__burger");
-    // const menu = document.querySelector(".header__col--menu");
-    // const header = document.querySelector("header");
+import {Swiper} from "swiper";
+import {Autoplay, Navigation, Pagination, Thumbs} from "swiper/modules";
 
-    // burger.addEventListener("click", () => {
-    //     burger.classList.toggle("active");
-    //     menu.classList.toggle("active");
-    //     document.body.classList.toggle("lock");
-    // });
+function updateCardSliders () { 
+    document.querySelectorAll(".slider-notloaded").forEach(slider => {
+        const swiperSlider = new Swiper(slider, {
+            autoplay: {
+                delay: 1500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".product-card__pagination",
+            },
+            loop: true,
+            modules: [Autoplay, Pagination],
+            effect: "fade",
+            on: {
+                init() {
+                    this.autoplay.stop();
+
+                    this.el.parentElement.addEventListener("mouseenter", () => {
+                        console.log("ok");
+                        this.autoplay.start();
+                    });
+                    
+                    this.el.parentElement.addEventListener("mouseleave", () => {
+                        console.log("ok2");
+                        this.autoplay.stop();
+                    })
+                }
+            }
+        });
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth <= 876) {
+        const burger = document.querySelector(".header__burger");
+        const menu = document.querySelector(".header__mobmenu");
+
+        burger.addEventListener("click", () => {
+            burger.classList.toggle("active");
+            menu.classList.toggle("active");
+        })
+    }
+
+    if (document.querySelector(".product-card")) {
+        const productCardsSlider = document.querySelectorAll(".product-card__slider"); 
+
+        updateCardSliders();
+    }
+
+    if (document.querySelector(".single__slider")) {
+
+        const singleNavSlider = new Swiper(".single__slider-nav", {
+            direction: "vertical",
+            slidesPerView: 3,
+            spaceBetween: 20,
+            modules: [Navigation],
+            navigation: {
+                nextEl: ".single__slider-nav-arrow--next",
+                prevEl: ".single__slider-nav-arrow--prev",
+            }
+
+        });
+        
+        const singleMainSlider = new Swiper(".single__slider-main", {
+            slidesPerView: 1,
+            modules: [Thumbs],
+            thumbs: {
+                el: singleNavSlider
+            }
+        });
+    }
 });
