ea580502af5457f73e88a6787132575978e5685f
Stanislav-PC <stanislav_2010@icloud.com>
2023-10-09 04:26:07 +0500

    Start

--- src/JS/index.js
+++ src/JS/index.js
@@ -52,17 +52,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (document.querySelector(".single__slider")) {
         const sliderNavSlides = document.querySelectorAll(".single__slider-nav-slide");
+        const sliderMainSlider = document.querySelectorAll(".single__slider-main-slide");
         const sliderMainVideo = document.querySelector(".single__slider-main-slide video");
-
+        const sliderNavPrev = document.querySelector(".single__slider-nav-arrow--prev");
+        const sliderNavNext = document.querySelector(".single__slider-nav-arrow--next");
+        const sliderNavWrapper = document.querySelector(".single__slider-nav .swiper-wrapper");
+        const sliderMainNext = document.querySelector(".single__slider-main-arrow--next");
+        const sliderMainPrev = document.querySelector(".single__slider-main-arrow--prev");
+        let sliderNavOffset = 0;
+        
         const singleNavSlider = new Swiper(".single__slider-nav", {
             direction: "vertical",
-            slidesPerView: 3,
+            slidesPerView: "auto",
+            allowSlideNext: false,
+            allowSlidePrev: false, 
             spaceBetween: 20,
-            modules: [Navigation],
-            navigation: {
-                nextEl: ".single__slider-nav-arrow--next",
-                prevEl: ".single__slider-nav-arrow--prev",
-            }
         });
         
         const singleMainSlider = new Swiper(".single__slider-main", {
@@ -91,13 +95,73 @@ document.addEventListener('DOMContentLoaded', function () {
                     if (sliderNavSlides[index].classList.contains("single__slider-nav-slide--video")) {
                         sliderMainVideo.play();
                     } else {
-                        console.log('dsa');
                         sliderMainVideo.pause();
                     }
-                }
+
+                },
             }
         });
+
+        sliderMainNext.addEventListener("click", () => {
+            sliderNavPrev.classList.remove("swiper-button-disabled");
+            if (sliderNavOffset <= ((sliderNavWrapper.scrollHeight - (sliderNavSlides[0].clientHeight + 20) * 2) * -1)) {
+                sliderNavNext.classList.add("swiper-button-disabled");
+            } else {
+                sliderNavOffset -= sliderNavSlides[0].clientHeight + 20;
+                sliderNavNext.classList.remove("swiper-button-disabled");
+            }
+            sliderNavWrapper.style.transform = `translateY(${sliderNavOffset}px)`;
+
+            console.log(sliderNavOffset);
+        })
+        
+        sliderMainPrev.addEventListener("click", () => {
+
+            sliderNavNext.classList.remove("swiper-button-disabled");
+            if (sliderNavOffset >= -2) {
+                sliderNavOffset = 0;
+                sliderNavPrev.classList.add("swiper-button-disabled");
+            } else {
+                sliderNavOffset += sliderNavSlides[0].clientHeight + 20;
+                sliderNavPrev.classList.remove("swiper-button-disabled");
+            }
+            sliderNavWrapper.style.transform = `translateY(${sliderNavOffset}px)`;
+            console.log(sliderNavSlides[0].clientHeight);
+
+        })
+
+        sliderNavNext.addEventListener("click", () => {
+            sliderNavPrev.classList.remove("swiper-button-disabled");
+            if (sliderNavOffset <= ((sliderNavWrapper.scrollHeight - (sliderNavSlides[0].clientHeight + 20) * 2) * -1)) {
+                sliderNavNext.classList.add("swiper-button-disabled");
+            } else {
+                sliderNavOffset -= sliderNavSlides[0].clientHeight + 20;
+                sliderNavNext.classList.remove("swiper-button-disabled");
+            }
+            sliderNavWrapper.style.transform = `translateY(${sliderNavOffset}px)`;
+
+        })
+        
+        sliderNavPrev.addEventListener("click", () => {
+            sliderNavNext.classList.remove("swiper-button-disabled");
+            if (sliderNavOffset === 0) {
+                sliderNavOffset = 0;
+                sliderNavPrev.classList.add("swiper-button-disabled");
+            } else {
+                sliderNavOffset += sliderNavSlides[0].clientHeight + 20;
+                sliderNavPrev.classList.remove("swiper-button-disabled");
+            }
+            sliderNavWrapper.style.transform = `translateY(${sliderNavOffset}px)`;
+
+        })
         
         sliderMainVideo.play();
+    
+        sliderNavSlides.forEach((slide, i) => {
+            slide.addEventListener("mouseover", (e) => {
+                singleNavSlider.slideTo(i);
+                singleMainSlider.slideTo(i);
+            })
+        })
     }
 });
